4dfc2908da03757f20b2d3e1011f9e52a032c2ee
Pasterian <lloydwalker2@gmail.com>
2016-05-27 13:44:14 +0100

    Initial commit, setting up mod references

--- src/io/github/scaling/guacamole/Main.java
+++ src/io/github/scaling/guacamole/Main.java
@@ -5,10 +5,26 @@ import org.bukkit.plugin.java.JavaPlugin;
 /**
  * Created by Toby on 19/05/2016.
  */
+
+
 public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        public static String VERSION;
+        public static String MOD_NAME;
+        public static String[] AUTHORS;
+
+        AUTHORS[0] = "Toby Wishart"; //Sorry for spelling! - L
+        AUTHORS[1] = "Lloyd Walker";
+
+        if (!Bukkit.getPluginManager().isPluginEnabled("scaling-guacamole")) {
+            getLogger().severe("*** scaling-guacamole is not installed or not enabled. ***");
+            getLogger().severe("*** This plugin will be disabled. ***");
+            this.setEnabled(false);
+            return;
+
+        }
 
     }
 
